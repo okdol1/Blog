9bbbc20149c46a58f28c127dc589a38352ecfae2
okdol1 <okdol0611@gmail.com>
2025-09-06 12:48:01 +0900

    css: 글 제목 `word-break: break-all;` 적용

--- app/(pages)/blog/[slug]/page.tsx
+++ app/(pages)/blog/[slug]/page.tsx
@@ -61,7 +61,7 @@ export default async function PostPage({
   return (
     <article className="grid mt-16 mb-20 sm:mb-60">
       <header className="pb-2.5">
-        <h1 className="text-[40px] font-bold mb-6">{data.title}</h1>
+        <h1 className="text-[40px] font-bold mb-6 break-all">{data.title}</h1>
 
         <div className="text-sm bg-black border-b space-y-2.5 pb-2.5 mb-2 border-[#262626]">
           <div className="grid grid-cols-[160px,auto] items-center">
